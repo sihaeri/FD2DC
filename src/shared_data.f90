47d859d0af359b343104a6a04dfe34b6f70c9176
sina_haery <sina_haery@yahoo.com>
2017-07-24 12:32:41 +0000

    shared_data.f90 edited online with Bitbucket

--- src/shared_data.f90
+++ src/shared_data.f90
@@ -1,3 +1,18 @@
+!    This file is part of FD2DC.
+!
+!    FD2DC is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    FD2DC is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with FD2DC.  If not, see <http://www.gnu.org/licenses/>.
+
 MODULE shared_data
 
 use cula_sparse_type
